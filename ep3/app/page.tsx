5c42e2fd8872636b79d69de13ad90acd9029e4f2
harshiltomar <harshiltomar20@gmail.com>
2026-04-08 16:02:59 +0530

    docs: fuck cursor

--- ep3/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Keyboard } from "@/components/keyboard";
-
-export default function Home() {
-  return (
-    <div className="h-screen bg-white flex justify-center">
-      <Keyboard/>
-    </div>
-  )
-}
--- ep45/app/page.tsx
+++
@@ -1,7 +0,0 @@
-import DarkModeBrainrot from "@/components/card";
-import { Grid } from "@/components/grid";
-import Image from "next/image";
-
-export default function Home() {
-  return <div className="bg-white"><Grid/></div>
-}
